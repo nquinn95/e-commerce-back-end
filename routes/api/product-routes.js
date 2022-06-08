bca149673402c61cde5a4b2aafea5cb0efe248f7
nick quinn <nick.quinn@stadium1.com>
2022-06-07 23:50:07 -0400

    added async to all product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try{
     productsData = await Category.findByPk(req.params.id, {
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -81,7 +81,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -122,7 +122,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     await Product.destroy({
