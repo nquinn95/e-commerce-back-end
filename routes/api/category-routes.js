a53c01b629c3f2a2936b120ab970e845e683e2dc
nick quinn <nick.quinn@stadium1.com>
2022-06-07 23:26:08 -0400

    category-routes has been created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,90 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try{
+    const categoriesData = await Category.findAll({
+      
   // be sure to include its associated Products
+      include: [{model: Product}],
+    });
+    //throws a 404 error if there is no data in categoriesData
+    if (!categoriesData){
+      res.status(404).json({message: 'But there was nothing there.'})
+    }
+    res.status(200).json(categoriesData);
+  } catch(err){
+    res.status(500).json(err);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try{
+  categoriesData = await Category.findByPk(req.params.id, {
   // be sure to include its associated Products
+    include: [{model: Product}],
+  })
+
+  if (!categoriesData){
+    res.status(404).json({message: 'But there was nothing there.'})
+    return; 
+  } 
+  res.status(200).json(categoriesData);
+} catch(err){
+  res.status(500).json(err);
+}
+
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoriesData = await Category.create(req.body);
+
+    res.status(200).json(categoriesData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{
+    await Category.update(
+      {
+        id: req.body.id,
+      },
+      {
+        where: req.params.categories_id,
+      }
+    )
+      res.status(200).json('Data update complete');
+  } catch(err){
+    if (!categoriesData){
+      res.status(404).json({message: 'But there was nothing there.'})
+      return; 
+    } 
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    }) .then(data){
+      res.status(200).json('You have destroyed the data');
+    };
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
